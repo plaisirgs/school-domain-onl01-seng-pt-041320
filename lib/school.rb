fa950e273e57a19fd39371df2b6a688c3ca18b7d
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-07 23:38:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] = [name]
+  end
+
+
+
+
+
+
+
+end
