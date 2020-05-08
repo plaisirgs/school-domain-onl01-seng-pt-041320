4455a638ee0aaf139125bf09578afbd225f20e09
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-08 00:30:42 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade].nil?
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(name)
+    @roster[name]
+  end
+
+  def sort
+    @roster.each_value do |names|
+      names.sort!
+    end
+  end
+
+
+
+end
